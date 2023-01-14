f3dcbcf8be1e7bd5a1b91d57b62a5fa44395c28c
Jace Isaeff <jace.isaeff@swri.org>
2023-01-13 21:12:07 -0600

    Changes made to support better workflow at work.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,14 +22,22 @@ vim.g.maplocalleader = " "
 --   visual_block_mode = "x",
 --   term_mode = "t",
 --   command_mode = "c",
+--
+-- Git --
+keymap("n", "<A-r>", ":Gitsigns reset_hunk<CR>", opts)
+keymap("n", "<leader>b", ":Gitsigns blame_line<CR>", opts)
 
 -- Normal --
 -- Better window navigation
-keymap("n",     "<C-h>", "<C-w>h", opts)
-keymap("n",     "<C-j>", "<C-w>j", opts)
-keymap("n",     "<C-k>", "<C-w>k", opts)
-keymap("n",     "<C-l>", "<C-w>l", opts)
-keymap("n", "<leader>v", ":vs<CR>", opts)
+-- keymap("n", "<leader>j", "<C-w>j", opts)
+-- keymap("n", "<leader>h", "<C-w>h", opts)
+-- keymap("n", "<leader>k", "<C-w>k", opts)
+-- keymap("n", "<leader>l", "<C-w>l", opts)
+-- keymap("n", "<leader>v", ":vs<CR>", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -38,8 +46,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<Tab>", ":bnext<CR>", opts)
+keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,11 +1,18 @@
+vim.cmd[[
+vim.lsp.start({
+	name = 'pylsp',
+	cmd = {'pylsp'},
+	root_dir = vim.fs.dirname(vim.fs.find({'setup.py', 'pyproject.toml'}, { upward = true })[1]),
+})
+]] 
 -- local lsp_installer = require("nvim-lsp-installer").setup {}
-local lspconfig = require("lspconfig")
-
-local function on_attach(client)
-
-end
-
-lspconfig.pyright.setup {on_attach = on_attach}
+--local lspconfig = require("lspconfig")
+--
+--local function on_attach(client)
+--
+--end
+--
+--lspconfig.pyright.setup {on_attach = on_attach}
 -- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
 -- or if the server is already installed).
 -- lsp_installer.on_server_ready(function(server)
