29185fe20b23cc553ba5b42fea818aa840a16b8b
Isaeff, Jace A. <jace.isaeff@swri.org>
2026-09-08 11:11:18 -0500

    Add finder updates

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -1,10 +1,15 @@
 return {
   {
-    "junegunn/fzf",
-    build = function()
-      vim.fn["fzf#install"]()
-    end,
-    cmd = "FZF",
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "nvim-mini/mini.icons" },
+    ---@module "fzf-lua"
+    ---@type fzf-lua.Config|{}
+    ---@diagnostic disable: missing-fields
+    opts = {}
+    ---@diagnostic enable: missing-fields
   },
   {
     "nvim-telescope/telescope.nvim",
