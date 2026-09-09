f81f18609d16ed3b948620eaf5afc3f851e0e435
Isaeff, Jace A. <jace.isaeff@swri.org>
2026-09-08 21:13:13 -0500

    Rework keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,37 +1,55 @@
 local opts = { noremap = true, silent = true }
-local map = vim.api.nvim_set_keymap
+local term_opts = { silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+function VisSelect()
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<ESC>', true, false, true), 'nx', false)
+    vim.api.nvim_exec('normal! gv"xy', true)
+    local txt = vim.api.nvim_exec([[echo getreg('x')]], true)
+    return txt
+end
+
+vim.api.nvim_create_user_command("LatLon", function()
+    local text = vim.fn.getreg("+"):gsub('[°%s]', '')
+    vim.api.nvim_put({text}, "c", true, true)
+end, {})
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+keymap("n", "<F1>", "<Nop>", opts)
+keymap("x", "<C-w>o", "<Nop>", opts)
 
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = " "
+vim.g.EasyMotion_leader_key = "\\"
 
--- No ops because I screw things up
-map("", "<Space>", "<Nop>", opts)
-map("n", "<F1>", "<Nop>", opts)
-map("x", "<C-w>o", "<Nop>", opts)
-
--- Refresh with F5
-map("n", "<F5>", ":checkt<CR>", opts)
-
--- Turn off highlight
-map("n", "<BS>", ":noh<CR>", opts)
-
--- My awesome motions
-map("n", "gl", "g_", opts)
-map("n", "gh", "^", opts)
-map("n", "gp", "%", opts)
-map("v", "gl", "g_", opts)
-map("v", "gh", "^", opts)
-map("v", "gp", "%", opts)
-
--- Window resizing
-map("n", "<C-Up>", ":resize -2<CR>", opts)
-map("n", "<C-Down>", ":resize +2<CR>", opts)
-map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
---map("n", "<Up>", "5k", opts)
---map("n", "<Down>", "5j", opts)
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
 --
+-- Set F5 to refresh buffers
+keymap("n", "<F5>", ":checkt<CR>", opts)
+
+keymap("n", "<BS>", ":noh<CR>", opts)
+keymap("n", "gl", "g_", opts)
+keymap("n", "gh", "^", opts)
+keymap("n", "gp", "%", opts)
+keymap("v", "gl", "g_", opts)
+keymap("v", "gh", "^", opts)
+keymap("v", "gp", "%", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
@@ -47,48 +65,107 @@ vim.keymap.set('n', '<leader>m', function()
     vim.api.nvim_command([[normal p]])
 end)
 
-vim.keymap.set("n", "<F3>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<F2>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gIc<Left><Left><Left><Left>]])
+vim.keymap.set("v", "<M-r>", [["qy:%s/<C-r>q/<C-r>q/gIc<Left><Left><Left><Left>]])
+
+vim.keymap.set('v', '<C-s>', function()
+    require('telescope.builtin').grep_string({search=VisSelect()})
+end)
 
 -- Navigate buffers
-map("n", "<Tab>", ":bnext<CR>", opts)
-map("n", "<S-Tab>", ":bprevious<CR>", opts)
-map("n", "L", ":tabnext<CR>", opts)
-map("n", "H", ":tabprevious<CR>", opts)
+keymap("n", "<Tab>", ":bnext<CR>", opts)
+keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
+keymap("n", "L", ":tabnext<CR>", opts)
+keymap("n", "H", ":tabprevious<CR>", opts)
+--keymap("n", "<leader><Tab>", ":tabnew<CR>", opts)
 
 -- Move text up and down
-map("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-map("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+keymap("n", "<leader>t", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>n", ":NvimTreeFocus<CR>", opts)
+
+-- FZF
+--keymap("n", "<C-f>", ":FZF --walker-root=/home/jisaeff/workspaces/HF2/<CR>", opts)
+
+-- Telescope
+-- This has a ripgrep dependency on it.
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts) --Live_grep
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts) --Live_grep
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts) --Live_grep
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts) --Live_grep
+keymap("n", "<leader>fs", ":Telescope lsp_document_symbols<CR>", opts)
+keymap("n", "<leader>f/", ":Telescope current_buffer_fuzzy_find<CR>", opts)
+
+-- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#find-files-using-ag
+
+--local builtin = require('telescope.builtin')
+--vim.keymap.set('n', '<leader>f/', builtin.live_grep({grep_open_files=vim.fn.expand("%:p")}))
+
+keymap("n", "<leader>Tt", ":TestNearest<CR>", opts)
+keymap("n", "<leader>Ts", ":TestSuite<CR>", opts)
+
+--keymap("n", "[q", ":cprevious<CR>", opts) --Live_grep
+--keymap("n", "]q", ":cnext<CR>", opts) --Live_grep
 
 -- Insert --
 -- Press jk fast to enter
-map("i", "jk", "<ESC>", opts)
+keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-map("v", "<A-j>", ":m .+1<CR>==", opts)
-map("v", "<A-k>", ":m .-2<CR>==", opts)
---map("v", "p", '"_dP', opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+--keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-map("x", "J", ":move '>+1<CR>gv-gv", opts)
-map("x", "K", ":move '<-2<CR>gv-gv", opts)
-map("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-map("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Easy config editing
-map("n", "<F7>", ":Vex ~/.config/nvim/", opts)
+--local emacs_opts = { noremap = false, silent = true }
+--keymap("i", "<C-a>", "<Home>", emacs_opts)
+--keymap("i", "<C-e>", "$", emacs_opts)
+--keymap("i", "<C-k>", "<Esc>ld$a", emacs_opts)
+--keymap("i", "<C-u>", "<Esc>d0xi", emacs_opts)
+--keymap("i", "<A-f>", "<Esc>lwi", emacs_opts)
+--keymap("i", "<A-b>", "<Esc>bi", emacs_opts)
+--keymap("i", "<A-x>", "<Esc>:", emacs_opts)
 
--- Term remaps
-map("t", "<C-w>h", "<C-\\><C-N><C-w>h", opts)
-map("t", "<C-w>j", "<C-\\><C-N><C-w>j", opts)
-map("t", "<C-w>k", "<C-\\><C-N><C-w>k", opts)
-map("t", "<C-w>l", "<C-\\><C-N><C-w>l", opts)
-map("t", "<F1>", "<C-\\><C-N>", opts)
 
-map("n", "<C-a>", "@q", opts)
-map("n", "<C-x>", "<cmd>!chmod +x %<CR>", opts)
+keymap("n", "<leader>lg", ":LazyGit<CR>", opts)
+
+keymap("n", "<F7>", ":Vex ~/.config/nvim/lua/user/", opts)
+
+-- Terminal --
+--Better terminal navigation
+keymap("n", "<leader>\\", ":FloatermToggle<CR>", opts)
+keymap("t", "<C-w>h", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-w>j", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-w>k", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-w>l", "<C-\\><C-N><C-w>l", term_opts)
+keymap("t", "<F1>", "<C-\\><C-N>", term_opts)
+
+keymap("n", "<M-a>", "@q", opts)
+keymap("n", "<C-x>", "<cmd>!chmod +x %<CR>", opts)
+
+vim.keymap.set("n", "<leader>rb", function()
+    --vim.api.nvim_cmd({cmd="black", args={"-l", "79", vim.fn.expand("%:p")}}, {})
+    vim.fn.system {"/home/jisaeff/workspaces/hf2-venv/bin/black", "-l", "79", vim.fn.expand("%:p")}
+    vim.api.nvim_command("checktime")
+end)
+
+vim.api.nvim_create_user_command("Date", function()
+    local date = os.date("%Y-%m-%d")
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_buf_set_text(0, row - 1, col + 1, row - 1, col + 1, {date})
+end, {})
+
+keymap('i', "<C-f>", "<Esc>gwwA", opts)
