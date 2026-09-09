4c759e81c1379d72155f924f5637e6cf408f8f01
Isaeff, Jace A. <jace.isaeff@swri.org>
2026-09-08 21:12:10 -0500

    Disable flash for a time

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "folke/flash.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      modes = {
-        search = { enabled = false },
-      },
-    },
-    keys = {
-      { "<leader>s", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash jump" },
-      { "<leader>S", function() require("flash").treesitter() end, mode = { "n", "x", "o" }, desc = "Flash treesitter" },
-    },
-  },
+  --{
+  --  "folke/flash.nvim",
+  --  event = { "BufReadPost", "BufNewFile" },
+  --  opts = {
+  --    modes = {
+  --      search = { enabled = false },
+  --    },
+  --  },
+  --  keys = {
+  --    { "<leader>s", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash jump" },
+  --    { "<leader>S", function() require("flash").treesitter() end, mode = { "n", "x", "o" }, desc = "Flash treesitter" },
+  --  },
+  --},
   { "unblevable/quick-scope" },
   { "justinmk/vim-sneak" },
 }
